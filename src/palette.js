f7b6839e290c319b3b3c693833e68af9c5932bba
JackOatley <jack.f.oatley@gmail.com>
2019-08-20 10:12:59 +0100

    Palette colors to use.

---
+++ src/palette.js
@@ -0,0 +1,18 @@
+/**
+ * Palette colors ordered from lightest to darkest.
+ *
+ * @example
+ * var [lightest, light, dark, darkest] = palette[0];
+ *
+ */
+export const palette = [
+
+	// Pink (Default).
+	[
+		[228, 228, 228],
+		[233, 194, 194],
+		[220, 155, 155],
+		[119, 54, 112]
+	]
+
+]
