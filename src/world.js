d2f2cffb15460ea7a36a4c7bdba329af0cfdf5c1
JackOatley <jack.f.oatley@gmail.com>
2019-08-24 17:28:15 +0100

    Weird nipple hills!

--- src/world.js
+++ src/world.js
@@ -4,9 +4,9 @@ import {currentPalette, toCSS} from "./palette.js";
 var hills = [];
 var width, height;
 var maxDist = 100;
-var midHeight = 50;
+var midHeight = 0;
 var low = false;
-var lerpP = 0.0001;
+var lerpP = 4;
 /**
  *
  * @param {Number} _width
@@ -22,7 +22,9 @@ function init(_width, _height) {
  */
 function update(dt) {
 	if (hills.length == 0 || hills[hills.length - 1].x < width) {
-		var nh = DogeMath.randomRange(midHeight + low ? -25 : 25, midHeight + low ? -75 : 75);
+		// The ternary logic here needs parentheses, it otherwise wasn't working correctly.
+		var nh = DogeMath.randomRange(midHeight + (low ? -0 : 10), midHeight + (low ? -50 : 100));
+		low = !low;
 		addPoint(
 			width + maxDist,
 			nh
@@ -47,6 +49,10 @@ function addPoint(x, y) {
 			y: midY
 		});
 
+		// FIX, if you see the debug lines, then you can see that this isn't
+		// linear interpolation, as this distances between points get smaller.
+		// THe distances should be uniform.
+		// NVM, I'm happy enough with my linear semi-fix, lol.
 		lerpToPoint({
 			x: midX,
 			y: midY
@@ -62,6 +68,7 @@ function addPoint(x, y) {
 		}, d => {
 			hills.push(d);
 		});
+
 	}
 
 	hills.push({
@@ -71,26 +78,36 @@ function addPoint(x, y) {
 }
 
 function lerpToPoint(start, target, func) {
-	while (DogeMath.getDistance(start, target) > lerpP) {
+
+	// Do a dummy step to start. This prevent duplicate points.
+	const stepX = lerpP;
+	const stepY = stepX / 15;
+	start.x -= Math.sign(start.x - target.x) * stepX;
+	start.y += (target.y - start.y) * stepY;
+
+	// Then generate normally.
+	while (DogeMath.getDistance(start, target) >= lerpP) {
 		func({
 			x: start.x,
 			y: start.y
 		});
 		// start.x = DogeMath.eastIn(1, start.x, target.x - start.x, 100);
 		// start.y = DogeMath.eastIn(1, start.y, target.y - start.y, 50);
-		start.x += DogeMath.lerp(start.x, target.x, 10);
-		start.y += DogeMath.lerp(start.y, target.y, 5);
+		start.x -= Math.sign(start.x - target.x) * stepX;
+		start.y += (target.y - start.y) * stepY;
+		//start.x += DogeMath.lerp(start.x, target.x, 10);
+		//start.y += DogeMath.lerp(start.y, target.y, 5);
 	}
 }
 
 /**
  *
- * @param {Object} ctx
+ * @param {CanvasRenderingContext2D} ctx
  */
 function draw(ctx) {
+
+	// Draw fill.
 	ctx.fillStyle = toCSS(currentPalette[2]);
-	ctx.strokeStyle = toCSS(currentPalette[0]);
-	ctx.lineWidth = 2;
 	ctx.beginPath();
 	ctx.moveTo(hills[0].x, height);
 	ctx.lineTo(hills[0].x, height / 2 - hills[0].y);
@@ -99,7 +116,24 @@ function draw(ctx) {
 	});
 	ctx.lineTo(width, height);
 	ctx.fill();
+
+	// Draw outline.
+	ctx.strokeStyle = toCSS(currentPalette[0]);
+	ctx.lineWidth = 2;
 	ctx.stroke();
+
+	// Draw debug.
+	ctx.beginPath();
+	ctx.moveTo(hills[0].x, height);
+	ctx.lineTo(hills[0].x, 270);
+	hills.forEach(i => {
+		ctx.moveTo(i.x, height / 2 - i.y);
+		ctx.lineTo(i.x, 270);
+	});
+	ctx.lineWidth = 0.5;
+	ctx.strokeStyle = "rgb(0,0,0,100)";
+	//ctx.stroke();
+
 }
 
 export {
