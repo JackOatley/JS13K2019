5463cbe71d4659e481f64c380e29cb34ad8380f3
JackOatley <jack.f.oatley@gmail.com>
2019-08-26 15:38:09 +0100

    House sprite.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {sprite_sun} from "./sprites/sprite_sun.js";
 import {spriteRobinHorizontal} from "./sprites/robin_horizontal.js";
 import {spriteRobinVertical} from "./sprites/robin_vertical.js";
 import {spriteTree1} from "./sprites/tree_1.js";
+import {spriteHouse} from "./sprites/house.js";
 
 Sprite.init();
 world.init(ctxCanvas.width, ctxCanvas.height, 5);
@@ -52,6 +53,7 @@ function render() {
 	spriteRobinHorizontal.draw(0, 50, 50, 1, 1, performance.now()/200, [...currentPalette[3], 255]);
 	spriteRobinVertical.draw(0, 100, 50, 1, 1, performance.now()/200, [...currentPalette[3], 255]);
 	spriteTree1.draw(0, 150, 50, 1, 1, performance.now()/200, [...currentPalette[3], 255]);
+	spriteHouse.draw(0, 200, 50, 1, 1, performance.now()/200, [...currentPalette[3], 255]);
 	world.draw(ctx);
 	Sprite.batchEnd();
 
